b9ae607049c8c6c2f0443f29ed5ea3bb9e2c8826
richard <richardrt13@gmail.com>
2021-02-17 23:05:27 -0300

    Utilizando query para filtragem

--- machine_learning_anime_synopsis.py
+++ machine_learning_anime_synopsis.py
@@ -19,15 +19,13 @@ generos = df.genre.str.get_dummies()
 
 df_animes = pd.concat([df, generos], axis=1)
 
-is_shounen = df_animes['Shounen']==1
+df_filtrado = df_animes.query("Shounen == 1")
 
-df_animes_shounen = df_animes[is_shounen]
-
-df_animes_shounen = df_animes_shounen.reset_index(drop= True)
+df_filtrado = df_filtrado.reset_index(drop= True)
 
 #Localizando o anime no qual vamos realizar a previsão com base na sinopse
 
-df_animes_shounen[df_animes_shounen['title'].astype(str).str.contains('Naruto')]
+df_filtrado[df_filtrado['title'].astype(str).str.contains('Naruto')]
 
 #Iniciando o processo de Bag of Words limpando a sinopse
 
@@ -40,11 +38,11 @@ def preprocess_text(text):
     msg_wo_sw = [word for word in tokens if word not in sw]
     return msg_wo_sw
 
-df_animes_shounen['clean_text'] = df_animes_shounen['synopsis'].apply(preprocess_text)
+df_filtrado['clean_text'] = df_filtrado['synopsis'].apply(preprocess_text)
 
-all_texts = df_animes_shounen.iloc[:, -1].apply(lambda x: " ".join(x))
+all_texts = df_filtrado.iloc[:, -1].apply(lambda x: " ".join(x))
 
-text =  df_animes_shounen.iloc[43:44, -1].apply(lambda x: " ".join(x))
+text =  df_filtrado.iloc[43:44, -1].apply(lambda x: " ".join(x))
 
 #Treinando o modelo com as palavras
 
@@ -76,9 +74,11 @@ df_f = df_f.iloc[1:]
 df_f = df_f.reset_index(drop= True)
 df_f.sort_values(by='result', ascending=False, inplace=True)
 
-df_naruto = pd.concat([df_animes_shounen, df_f], axis=1)
-df_naruto.sort_values(by='result', ascending=False, inplace=True)
-df_naruto[['title','result','score']].head(50)
+df_result = pd.concat([df_filtrado, df_f], axis=1)
+df_result = df_result.query("score >= 7")
+df_result = df_result.query("members >= 400000")
+df_result.sort_values(by='result', ascending=False, inplace=True)
+df_result[['title','result','score','members']].head(50)
 
 
 
